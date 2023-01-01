5fb237853320a3c2aa13ae8e86a2bcbc2bd64dda
mantra1-1 <36801221+mantra1-1@users.noreply.github.com>
2023-01-01 05:57:18 -0800

    Add files via upload

---
+++ 20230101_numbers.py
@@ -0,0 +1,31 @@
+# number go brrrrrr
+
+number = 5
+print(number)
+
+
+# if I want to mess with the value
+
+# option 1 : simply do the math
+number = 5
+number = number + 3
+print(number)
+
+number = 5
+number = number * 2
+print(number)
+
+# option 2 : since math is so common, there are shortcuts
+number = 5
+number += 3
+print(number)
+
+number = 5
+number *= 2
+print(number)
+
+
+
+
+
+
